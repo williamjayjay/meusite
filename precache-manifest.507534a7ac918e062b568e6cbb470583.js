4e53c0445967942de570a25d5c62d05bda9cc8ba
William Gomes <52439569+williamjayjay@users.noreply.github.com>
2021-05-12 23:54:03 -0300

    Initial commit

---
+++ precache-manifest.507534a7ac918e062b568e6cbb470583.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0051f87b50fd84bb77d42511be4fcf07",
+    "url": "./index.html"
+  },
+  {
+    "revision": "281a30fdd1629dbd6864",
+    "url": "./static/css/main.474ddbc6.chunk.css"
+  },
+  {
+    "revision": "db6914830cb37bfef90c",
+    "url": "./static/js/2.3aaeb56f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.3aaeb56f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "281a30fdd1629dbd6864",
+    "url": "./static/js/main.6bec61c3.chunk.js"
+  },
+  {
+    "revision": "d01390bccfd28b4f5290",
+    "url": "./static/js/runtime-main.c0ec3810.js"
+  }
+]);
